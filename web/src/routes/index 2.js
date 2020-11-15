66d36d82bde848d5b058690e1347dbde55ef5bb6
Uzziel Perez <uzzie.perez@cern.ch>
2020-11-15 08:50:19 +0100

    integrate

--- web/src/routes/index 2.js
+++ web/src/routes/index 2.js
@@ -4,12 +4,11 @@ import { TipsTricks } from "../components/TipsAndTricks";
 import { Home } from "../components/Home";
 import { Resources } from "../components/Resources";
 
-
 export default function Routes() {
   return (
     <AppBar>
       <Switch>
-        <Route exact path="/" exact component={Home} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/tips-tricks" component={TipsTricks} />
         <Route exact path="/resources" component={Resources} />
       </Switch>
