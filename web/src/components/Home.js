f0aebac9ae54cf54981e3b15d3201623318e09b4
agarwalmanya <manya18ag@gmail.com>
2020-11-15 20:34:51 +0800

    web: edit homepage

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -1,68 +1,94 @@
-import { Row, Col, Button } from "antd";
+import { Row, Col, Button, Typography } from "antd";
 import { GithubOutlined, TranslationOutlined } from "@ant-design/icons";
 import { Redirect } from "react-router-dom";
 import Background from "../assets/Images Website/Background.png";
+import Splash from "../assets/Images Website/Splash.png";
 
 export const Home = () => {
   return (
-    <Row
-      style={{
-        // height:"80vh",
-        // width:"100%",
-        flexDirection: "row-reverse",
-        justifyContent: "flex-start",
-        margin: "10em 3em",
-      }}
-    >
-      <Col
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          minWidth: "200px",
-        }}
-      >
-        <Button
+    <div>
+      <Row style={{ flexDirection: "column", justifyContent: "center" }}>
+        <div
           style={{
-            backgroundColor: "#31abc0",
-            color: "white",
-            border: "0",
-            width: "150px",
-            height: "auto",
-            padding: "10px",
-            fontSize: "1.2em",
-            marginRight: "1em",
+            background: `url(${Splash})`,
+            height: "80vh",
+            width: "100%",
+            backgroundSize: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
           }}
-          icon={<GithubOutlined />}
-          onClick={() =>
-            window.open(
-              "https://github.com/particlesilo/particlesilo",
-              "_blank"
-            )
-          }
         >
-          Contribute
-        </Button>
-        <Button
-          style={{
-            backgroundColor: "#31abc0",
-            color: "white",
-            border: "0",
-            width: "150px",
-            height: "auto",
-            padding: "10px",
-            fontSize: "1.2em",
-          }}
-          icon={<TranslationOutlined />}
-          onClick={() =>
-            window.open(
-              "https://github.com/particlesilo/particlesilo/blob/main/README.md",
-              "_blank"
-            )
-          }
-        >
-          Translate
-        </Button>
-      </Col>
-    </Row>
+          <Typography.Title style={{ fontSize: "5em", marginBottom:"0" }}>
+            Particle Silo
+          </Typography.Title>
+          <Typography.Title style={{ fontSize: "3em" }}>
+            The best Science Communication materials for your next outreach
+            activities in one place!{" "}
+          </Typography.Title>
+          <Row
+            style={{
+              // height:"80vh",
+              // width:"100%",
+              flexDirection: "row-reverse",
+              justifyContent: "center",
+              // margin: "10em 3em",
+            }}
+          >
+            <Col
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                minWidth: "200px",
+              }}
+            >
+              <Button
+              type="primary"
+                style={{
+                  // backgroundColor: "#31abc0",
+                  color: "white",
+                  border: "0",
+                  width: "150px",
+                  height: "auto",
+                  padding: "10px",
+                  fontSize: "1.2em",
+                  marginRight: "1em",
+                }}
+                icon={<GithubOutlined />}
+                onClick={() =>
+                  window.open(
+                    "https://github.com/particlesilo/particlesilo",
+                    "_blank"
+                  )
+                }
+              >
+                Contribute
+              </Button>
+              <Button
+              type="primary"
+                style={{
+                  // backgroundColor: "#31abc0",
+                  color: "white",
+                  border: "0",
+                  width: "150px",
+                  height: "auto",
+                  padding: "10px",
+                  fontSize: "1.2em",
+                }}
+                icon={<TranslationOutlined />}
+                onClick={() =>
+                  window.open(
+                    "https://github.com/particlesilo/particlesilo/blob/main/README.md",
+                    "_blank"
+                  )
+                }
+              >
+                Translate
+              </Button>
+            </Col>
+          </Row>
+        </div>
+      </Row>
+    </div>
   );
 };
--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -8,18 +8,20 @@ import { VideoResources } from "../components/VideoResources";
 import { PresentationResources } from "../components/PresentationResources";
 import { UploadForm } from "../components/UploadForm";
 
-
 export default function Routes() {
   return (
     <AppBar>
       <Switch>
+        <Route path="/upload" component={UploadForm} />
+        <Route path="/tips-tricks" component={TipsTricks} />
+        <Route path="/resources/images" component={ImageResources} />
+        <Route path="/resources/videos" component={VideoResources} />
+        <Route
+          path="/resources/presentations"
+          component={PresentationResources}
+        />
+        <Route path="/resources" component={Resources} />
         <Route exact path="/" component={Home} />
-        <Route exact path="/upload" component={UploadForm}/>
-        <Route exact path="/tips-tricks" component={TipsTricks} />
-        <Route exact path="/resources" component={Resources} />
-        <Route exact path="/resources/images" component={ImageResources} />
-        <Route exact path="/resources/videos" component={VideoResources} />
-        <Route exact path="/resources/presentations" component={PresentationResources} />
       </Switch>
     </AppBar>
   );
